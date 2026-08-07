6ba4e15aa9ca6d268b8d1f21fe9c3df3689de0ef
Snap-sec <support@snapsec.co>
2026-08-07 18:24:37 +0530

    Optimize scrolling performance and remove duplicate scripts

--- assets/js/mediumish.js
+++ assets/js/mediumish.js
@@ -1,129 +1,70 @@
-jQuery(document).ready(function($){
+/**
+ * Modern High-Performance Script for Snapsec Blog
+ * Uses requestAnimationFrame throttling and passive event listeners for silky smooth scrolling.
+ */
+document.addEventListener('DOMContentLoaded', function() {
+    'use strict';
 
-    //fix for stupid ie object cover
-    if (document.documentMode || /Edge/.test(navigator.userAgent)) {
-      jQuery('.featured-box-img-cover').each(function(){
-          var t = jQuery(this),
-              s = 'url(' + t.attr('src') + ')',
-              p = t.parent(),
-              d = jQuery('<div></div>');
-  
-          p.append(d);
-          d.css({
-              'height'                : '290',
-              'background-size'       : 'cover',
-              'background-repeat'     : 'no-repeat',
-              'background-position'   : '50% 20%',
-              'background-image'      : s
-          });
-          t.hide();
-      });
-    }
+    // 1. Optimized Header Scroll Hide/Show using GPU-accelerated transforms
+    var navbar = document.querySelector('nav.mediumnavigation');
+    if (navbar) {
+        var lastScrollTop = 0;
+        var delta = 10;
+        var ticking = false;
 
-    // alertbar later
-    $(document).scroll(function () {
-        var y = $(this).scrollTop();
-        if (y > 280) {
-            $('.alertbar').fadeIn();
-        } else {
-            $('.alertbar').fadeOut();
-        }
-    });
+        function updateNavbar() {
+            var st = window.pageYOffset || document.documentElement.scrollTop;
+            
+            // Avoid negative scrolling bounce (iOS)
+            if (st < 0) st = 0;
 
-
-    // Smooth on external page
-    $(function() {
-      setTimeout(function() {
-        if (location.hash) {
-          /* we need to scroll to the top of the window first, because the browser will always jump to the anchor first before JavaScript is ready, thanks Stack Overflow: http://stackoverflow.com/a/3659116 */
-          window.scrollTo(0, 0);
-          target = location.hash.split('#');
-          smoothScrollTo($('#'+target[1]));
-        }
-      }, 1);
-
-      // taken from: https://css-tricks.com/snippets/jquery/smooth-scrolling/
-      $('a[href*=\\#]:not([href=\\#])').click(function() {
-        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-          smoothScrollTo($(this.hash));
-          return false;
-        }
-      });
-
-      function smoothScrollTo(target) {
-        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-
-        if (target.length) {
-          $('html,body').animate({
-            scrollTop: target.offset().top
-          }, 1000);
-        }
-      }
-    });
-    
-    
-    // Hide Header on on scroll down
-    var didScroll;
-    var lastScrollTop = 0;
-    var delta = 5;
-    var navbarHeight = $('nav').outerHeight();
-
-    $(window).scroll(function(event){
-        didScroll = true;
-    });
-
-    setInterval(function() {
-        if (didScroll) {
-            hasScrolled();
-            didScroll = false;
-        }
-    }, 250);
-
-    function hasScrolled() {
-        var st = $(this).scrollTop();
-        
-        // Make sure they scroll more than delta
-        if(Math.abs(lastScrollTop - st) <= delta)
-            return;
-
-        // If they scrolled down and are past the navbar, add class .nav-up.
-        // This is necessary so you never see what is "behind" the navbar.
-        if (st > lastScrollTop && st > navbarHeight){
-            // Scroll Down            
-            $('nav').removeClass('nav-down').addClass('nav-up'); 
-            $('.nav-up').css('top', - $('nav').outerHeight() + 'px');
-           
-        } else {
-            // Scroll Up
-            if(st + $(window).height() < $(document).height()) {               
-                $('nav').removeClass('nav-up').addClass('nav-down');
-                $('.nav-up, .nav-down').css('top', '0px');             
+            if (Math.abs(lastScrollTop - st) > delta) {
+                if (st > lastScrollTop && st > 100) {
+                    // Scrolling down - hide navbar smoothly
+                    navbar.classList.add('nav-hidden');
+                    navbar.classList.remove('nav-visible');
+                } else {
+                    // Scrolling up - show navbar
+                    navbar.classList.remove('nav-hidden');
+                    navbar.classList.add('nav-visible');
+                }
+                lastScrollTop = st;
             }
+            ticking = false;
         }
 
-        lastScrollTop = st;
+        window.addEventListener('scroll', function() {
+            if (!ticking) {
+                window.requestAnimationFrame(updateNavbar);
+                ticking = true;
+            }
+        }, { passive: true });
     }
-        
-    $('.site-content').css('margin-top', $('header').outerHeight() + 'px');  
-    
-    // spoilers
-     $(document).on('click', '.spoiler', function() {
-        $(this).removeClass('spoiler');
-     });
-    
- });   
 
-// deferred style loading
-var loadDeferredStyles = function () {
-	var addStylesNode = document.getElementById("deferred-styles");
-	var replacement = document.createElement("div");
-	replacement.innerHTML = addStylesNode.textContent;
-	document.body.appendChild(replacement);
-	addStylesNode.parentElement.removeChild(addStylesNode);
-};
-var raf = window.requestAnimationFrame || window.mozRequestAnimationFrame ||
-	window.webkitRequestAnimationFrame || window.msRequestAnimationFrame;
-if (raf) raf(function () {
-	window.setTimeout(loadDeferredStyles, 0);
+    // 2. Throttled Alertbar Toggle
+    var alertbar = document.querySelector('.alertbar');
+    if (alertbar) {
+        var alertTicking = false;
+        window.addEventListener('scroll', function() {
+            if (!alertTicking) {
+                window.requestAnimationFrame(function() {
+                    var y = window.pageYOffset || document.documentElement.scrollTop;
+                    if (y > 280) {
+                        alertbar.classList.add('show');
+                    } else {
+                        alertbar.classList.remove('show');
+                    }
+                    alertTicking = false;
+                });
+                alertTicking = true;
+            }
+        }, { passive: true });
+    }
+
+    // 3. Spoiler click handler
+    document.addEventListener('click', function(e) {
+        if (e.target && e.target.classList.contains('spoiler')) {
+            e.target.classList.remove('spoiler');
+        }
+    });
 });
-else window.addEventListener('load', loadDeferredStyles);
